30408183316e505d4b05ef3a75cffc48e917f5f6
Hope-Michael <54977611+Hope-Michael@users.noreply.github.com>
2020-04-22 13:02:04 +0300

    Update DBConnector.php

--- labs/DBConnector.php
+++ labs/DBConnector.php
@@ -1,20 +1,20 @@
-<?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'btc3205');
+<?php 
+define('DB_SERVER','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('Db_NAME','btc3205');
 
-// Creating connection
 class DBConnector{
-	public $conn;
+    public $conn;
 
-		function _construct(){
-		$this->conn=mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die("Error:" .mysql_error());
-		mysql_select_db(DB_NAME, $this->conn);
-	}
-	public function closeDatabase(){
-		mysql_close($this->conn);
-	}
+    function __construct()
+    {
+        $this->conn=mysql_connect(DB_SERVER, DB_USER, DB_PASS) or die("Error:" .mysql_error());
+		mysql_select_db(Db_NAME, $this->conn);
+    }
+
+    public function closeDatabase(){
+        mysqli_close($this->conn);
+    }
 }
+?>
